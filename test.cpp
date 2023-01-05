6021e8925ef7d4dad57bca71689979734bdb0287
noahyacoub123 <97934960+noahyacoub123@users.noreply.github.com>
2023-01-04 23:03:12 -0500

    Add files via upload
    This program asks the user for two numbers and then returns their sum

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/*words*/
+#include<iostream>
+using namespace std;
+
+int main()
+{  
+    float num1;
+    float num2;
+    float sum;
+
+    cout<<"please enter two numbers:"<<endl; // cout is c out, is an output
+    cin>>num1>>num2; // cin is c in, means it takes an input
+    sum = num1 + num2;
+    cout<<num1<<" + "<<num2<<" = "<<sum<<endl;
+
+    return 0;
+}
